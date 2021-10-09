3e38500105e298a2e49785a376433dab9ec419b0
dmrpushkar <rpushkar.beis.15@acharya.ac.in>
2021-10-09 22:05:25 +0530

    add firestore

--- src/firestore.js
+++ src/firestore.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import * as db from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, where, query, getDocs } from "firebase/firestore";
 
 const app = initializeApp({
   apiKey: "AIzaSyAhbhz0o76LrVR0heh9AczAU-L3-SUh-v8",
@@ -10,12 +10,17 @@ const app = initializeApp({
   appId: "1:1024455669331:web:49b43d4043de12f6b178a6",
 });
 
+const db = getFirestore(app);
+
 const getBookings = async () => {
-  const q = db.query(db.collection(db, "bookings"), db.where("name", "==", "Pushkar R"));
-  const querySnapshot = await db.getDocs(q);
+  const q = query(collection(db, "bookings"), where("name", "==", "Pushkar R"));
+  const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
     console.log(doc.id, " => ", doc.data());
   });
 }; 
 
+export {
+  getBookings,
+}
