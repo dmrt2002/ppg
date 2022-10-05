de3040f9e72ead2ace68c4fb09be96e8639d28d2
R Tushar <dmrtushar@gmail.com>
2022-10-05 19:29:43 +0530

    third

--- src/views/BookingPage/BookingPage.js
+++ src/views/BookingPage/BookingPage.js
@@ -8,7 +8,7 @@ import Camera from "@material-ui/icons/Camera";
 import Palette from "@material-ui/icons/Palette";
 import Favorite from "@material-ui/icons/Favorite";
 // core components
-import Header from "components/Header/Header.js";
+import Header from "components/Header/Header.js"
 import Footer from "components/Footer/Footer.js";
 import Button from "components/CustomButtons/Button.js";
 import GridContainer from "components/Grid/GridContainer.js";
